29c5875e70d3375eb1351944c2801027b5691860
Ian Estrella <107520125+youraveragedeveloper7@users.noreply.github.com>
2025-04-22 20:16:07 -0700

    Finished the project.

--- public/index.js
+++ public/index.js
@@ -112,9 +112,11 @@ const orderDetails = {
   },
   process() {
     /* WRITE YOUR CODE HERE - Then remove the console.log */
-    console.log(
-      `Bug #3: Write a METHOD that depletes the inventory by using the 'items' array in the orderDetails object.`
-    )
+    this.items.forEach(({itemname, count}) => {
+      for (let [ingredient, amount] of Object.entries(mealIngredients[itemname])){
+        inventory[ingredient].qty -= count * amount
+      }
+    })
   },
 }
 
@@ -125,9 +127,7 @@ const orderDetails = {
 
 const restockAndDisplay = () => {
   /* WRITE YOUR CODE HERE - Then remove the console.log */
-  console.log(
-    "Bug #2: Invoke the inventory object METHOD that restocks according to prescribed supply levels for each ingredient. Be sure to include the required (object) argument."
-  )
+  inventory.restock(resupplyLevels)
   inventory.show()
   historyLog(
     `Restocked at ${new Date().toLocaleString("en-US", {
@@ -191,8 +191,6 @@ myForm.addEventListener("submit", formSubmit)
 
 document.addEventListener("DOMContentLoaded", () => {
   /* WRITE YOUR CODE HERE - Then remove the console.log */
-  console.log(
-    `Bug #1: Call the STANDALONE function that kicks off restocking and displaying of inventory.`
-  )
+  restockAndDisplay()
   document.querySelector("#message").innerHTML = ""
 })
